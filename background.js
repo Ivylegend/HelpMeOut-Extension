418ff219dfdf17ae7d1845127f87883eb141bafb
Ivylegend <victoryicha2@gmail.com>
2023-09-29 23:27:48 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,17 @@
+// chrome
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
+        files: ["/content.js"],
+      })
+      .then(() => {
+        console.log("weve injeccted the content script");
+      })
+      .catch((err) => console.log(err, "error in background"));
+  }
+});
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{})
---
+++ js/popup.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // GET THE SELECTORS OF THE BUTTONS
+  const startVid = document.querySelector("button#start-vid");
+  const stopVid = document.querySelector("button#stop-vid");
+  // const popup = document.querySelector("#popup");
+  // const closePopup = document.querySelector("#close");
+
+  //   ADDING EVENT LISTENERS
+
+  startVid.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "request_recording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error on line 14");
+          }
+        }
+      );
+    });
+  });
+
+  // closePopup.addEventListener("click", () => {
+  //   popup.style.display = "none";
+  // });
+
+  stopVid.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "stopVideo" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error on line 27");
+          }
+        }
+      );
+    });
+  });
+});
